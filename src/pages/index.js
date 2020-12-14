8c7f972b7dc3f2b58058b51629da98dea9b4f5a1
Justin Juno <hello@justinjuno.dev>
2020-12-14 11:05:32 -0600

    update header content

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,9 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Header title="Hello, World!">
       My name is Justin Juno. I'm an experienced Frontend Developer and UI
-      Designer seeking career opportunities. I'd love to join a talented team of
-      diverse engineers who value aesthetics, accessibility, and mentorship.
-      Here you can learn more about me, peruse the beginnings of a blog, and
-      explore my playground.
+      Designer seeking career opportunities. Here you can learn more about me,
+      peruse the beginnings of a blog, and explore my playground. Thanks for
+      stopping by!
     </Header>
     <h2>Blog.</h2>
     <PostFeed />
