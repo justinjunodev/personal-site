33a9a2363a0903d248829e4bfc279633e54e55ad
Justin Juno <hello@justinjuno.dev>
2020-12-28 12:22:50 -0600

    add fathom analytics

---
+++ src/html.js
@@ -0,0 +1,46 @@
+/* eslint-disable react/no-danger */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/html-has-lang */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+        <script
+          src="https://cdn.usefathom.com/script.js"
+          data-spa="auto"
+          data-site="BULMVLTE"
+          defer
+        />
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key="body"
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
